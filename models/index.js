38f80acade7baa3fc5ed7ee172d73de856dcb31b
sshahram <shirin.shahram23@gmail.com>
2021-04-20 14:48:54 -0700

    define model associations

--- models/index.js
+++ models/index.js
@@ -1,5 +1,16 @@
 const User = require('./User');
 const Post = require('./Post');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// make reverse association
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = {
     User,
     Post
