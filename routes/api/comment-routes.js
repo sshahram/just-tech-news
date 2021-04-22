ad0d373390bdce71550373ac04f3b81427a5abe9
sshahram <shirin.shahram23@gmail.com>
2021-04-22 10:30:49 -0700

    add get and post routes for comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 const {Comment} = require('../../models');
 
 router.get('/', (req, res) => {
-
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.post('/', (req, res) => {
@@ -19,7 +24,22 @@ router.post('/', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if(!dbCommentData) {
+                res.status(404).json({message: 'No comment found with this id'});
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
 });
 
 module.exports = router;
