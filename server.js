cfb277f6abe781625c2cfcfb98a48799dc05f857
sshahram <shirin.shahram23@gmail.com>
2021-04-26 23:09:01 -0700

    Set up the template engine

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +14,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+// app's template engine of choice
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({force: false}).then(() => {
