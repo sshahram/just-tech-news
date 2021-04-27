a0ea56f60b280769e4b6864604bf4f1352b3764a
sshahram <shirin.shahram23@gmail.com>
2021-04-27 14:23:18 -0700

    create a session on the back end

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,7 +63,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })    
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -90,7 +98,15 @@ router.post('/login', (req, res) => {
             res.status(400).json({message: 'Incorrect password!'});
             return;
         }
-        res.json({user: dbUserData, message: 'You are now logged in!'});
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({user: dbUserData, message: 'You are now logged in!'});
+        });
     });
 });
 
--- server.js
+++ server.js
@@ -4,13 +4,25 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
