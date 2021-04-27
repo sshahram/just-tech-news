75b5237cb4533c21f934a5085e278c4816c223a7
sshahram <shirin.shahram23@gmail.com>
2021-04-26 23:53:24 -0700

    populate template with Sequelize data

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,41 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const{Post, User, Comment} = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/',
-        title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments:[{}, {}],
-        user: {
-            username: 'test_user'
-        }
-    });
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // pass a single post object into the homepage template
+            // console.log(dbPostData[0]);
+            const posts =dbPostData.map(post => post.get({plain: true}));
+            res.render('homepage', {posts});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
