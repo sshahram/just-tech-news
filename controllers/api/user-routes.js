8d86233004441b4882eab0615080ea9b66b568c5
sshahram <shirin.shahram23@gmail.com>
2021-04-27 14:57:01 -0700

    add logic to destroy the session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -110,6 +110,18 @@ router.post('/login', (req, res) => {
     });
 });
 
+// route for logout
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
