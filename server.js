1f1b9cc38902af694af3df871ffcd899258e42d7
sshahram <shirin.shahram23@gmail.com>
2021-04-28 00:20:47 -0700

    modify session

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: 'secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
